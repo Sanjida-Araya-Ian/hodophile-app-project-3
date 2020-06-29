22031dcde49a631a550fd10767e6f8ad84ed3618
Araya Nawrath <arayanawrath@gmail.com>
2020-06-29 11:57:08 -0400

    correct spelling

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/hotelserch",
+  process.env.MONGODB_URI || "mongodb://localhost/hotelsearch",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
