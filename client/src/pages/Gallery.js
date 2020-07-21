b42863eb50e97bd5b18ccd240d525236d24a07a6
Ian Edwards <ian.edward@uconn.edu>
2020-07-21 00:44:36 -0400

    Added Gallery Page
    initiated gallery page
    updated export value
    added gallery images to public/img/gallery
    updated links

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -6,9 +6,9 @@ const divStyle = {
     backgroundImage: 'url(/img/cd.jpeg)',
     width: '100vw',
     height: '120vh'
-  };
+};
 
-function About() {
+function Gallery() {
     return (
         <>
             <div style={divStyle}>
@@ -20,51 +20,60 @@ function About() {
                     </Row>
                     <Row>
                         <Grid className="projects-grid">
-                            <Cell col={4}>
+                            <Cell col={3}>
                                 <Card shadow={5} style={{ minWidth: '300px', margin: 'auto' }}>
                                     <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/mee.jpg) center / cover' }}>
-                                        Araya Nawrath
-                    </CardTitle>
+                                    </CardTitle>
                                     <CardText>
-                                        Experienced Front-End/UI professional. Has personally developed applications used around the world.
+                                        Sorrento, Italy, 2016
                    </CardText>
                                     <CardActions border>
-                                        <Button colored href='https://github.com/My-unicorn'>Github</Button>
-                                        <Button colored href='http://www.arayanawrath.com/'>Website</Button>
+                                        <Button colored img src={"./img/gallery/google.png"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
                                     </CardMenu>
                                 </Card>
                             </Cell>
-                            <Cell col={4}>
+                            <Cell col={3}>
                                 <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
                                     <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/sanjida_nawrin.jpg) center / cover' }}>
-                                        Sanjida Nawrin
-               </CardTitle>
+                                    </CardTitle>
                                     <CardText>
                                         Expert in React.JS as well as a skilled back-end developer.
                   </CardText>
                                     <CardActions border>
-                                        <Button colored href='https://github.com/sn-1'>Github</Button>
-                                        {/* //<Button colored>Website</Button> */}
+                                        <Button colored img src={"./img/gallery/google.png"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
                                     </CardMenu>
                                 </Card>
                             </Cell>
-                            <Cell col={4}>
+                            <Cell col={3}>
                                 <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
                                     <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/Me.jpg) center / cover' }}>
-                                        Ian Edwards
-                 </CardTitle>
+                                    </CardTitle>
                                     <CardText>
                                         Highly trained full-stack developer fluent in Node.JS and server setup. Free agent able to do any task required.
                 </CardText>
                                     <CardActions border>
-                                        <Button colored href='https://github.com/ianedwards-jpg'>Github</Button>
-                                        <Button colored href='https://ianedwards.dev'>Website</Button>
+                                        <Button colored img src={"./img/gallery/google.png"}>View Full Size</Button>
+                                    </CardActions>
+                                    <CardMenu style={{ color: '#fff' }}>
+                                        <IconButton name='share' />
+                                    </CardMenu>
+                                </Card>
+                            </Cell>
+                            <Cell col={3}>
+                                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                                    <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/Me.jpg) center / cover' }}>
+                                    </CardTitle>
+                                    <CardText>
+                                        Highly trained full-stack developer fluent in Node.JS and server setup. Free agent able to do any task required.
+                </CardText>
+                                    <CardActions border>
+                                        <Button colored img src={"./img/gallery/google.png"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
@@ -80,4 +89,4 @@ function About() {
     );
 };
 
-export default About;
+export default Gallery;
