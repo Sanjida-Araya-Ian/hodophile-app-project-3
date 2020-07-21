96e5ebe1d52a13edeee81a16756595cd4e4bbd93
Ian Edwards <ian.edward@uconn.edu>
2020-07-21 10:42:51 -0400

    updates
    brought gallery page online
    fixed layout gallery js
    fixed links gallery js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Register from "./pages/Register";
 import Modal from "./components/Modal";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-// import Gallery from "./pages/Gallery";
+import Gallery from "./pages/Gallery";
 import Navbar from "./components/Navbar"
 import Location from "./pages/Location";
 import Footer from "./components/Footer";
@@ -59,7 +59,7 @@ class App extends Component{
           render={(props) => <Register {...props} login={this.logIn}/>}
           />
           <Route exact path="/location" component={Location}/>
-          {/* <Route exact path="/gallery" component={Gallery}/> */}
+          <Route exact path="/gallery" component={Gallery}/>
           <Route exact path="/about" component={About}/>
           <Route component={NoMatch} />
         </Switch>
--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -15,20 +15,20 @@ function Gallery() {
                 <Container className="my-5">
                     <Row>
                         <Col data-aos="fade-up" className="m-4 default">
-                            <h1>Meet our developers...</h1>
+                            <h1>Gallery</h1>
                         </Col >
                     </Row>
                     <Row>
                         <Grid className="projects-grid">
                             <Cell col={3}>
-                                <Card shadow={5} style={{ minWidth: '300px', margin: 'auto' }}>
-                                    <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/mee.jpg) center / cover' }}>
+                                <Card shadow={5} style={{ maxWidth: '250px', margin: 'auto' }}>
+                                    <CardTitle style={{ height: '300px', width: '250px', color: 'white', background: 'url(/img/gallery/sorrentosunset.jpg) center / cover' }}>
                                     </CardTitle>
                                     <CardText>
                                         Sorrento, Italy, 2016
                    </CardText>
                                     <CardActions border>
-                                        <Button colored img src={"./img/gallery/sorrentosunset.jpg"}>View Full Size</Button>
+                                        <Button colored href={"./img/gallery/sorrentosunset.jpg"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
@@ -36,14 +36,14 @@ function Gallery() {
                                 </Card>
                             </Cell>
                             <Cell col={3}>
-                                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                    <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/sanjida_nawrin.jpg) center / cover' }}>
+                                <Card shadow={5} style={{ maxWidth: '250px', margin: 'auto' }}>
+                                    <CardTitle style={{ height: '300px', width: '250px', color: 'white', background: 'url(/img/gallery/positanoitaly.jpg) center / cover' }}>
                                     </CardTitle>
                                     <CardText>
                                     Positano, Italy, 2016
                   </CardText>
                                     <CardActions border>
-                                        <Button colored img src={"./img/gallery/positanoitaly.jpg"}>View Full Size</Button>
+                                        <Button colored href={"./img/gallery/positanoitaly.jpg"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
@@ -51,14 +51,14 @@ function Gallery() {
                                 </Card>
                             </Cell>
                             <Cell col={3}>
-                                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                    <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/Me.jpg) center / cover' }}>
+                                <Card shadow={5} style={{ maxWidth: '250px', margin: 'auto' }}>
+                                    <CardTitle style={{ height: '300px', width: '250px', color: 'white', background: 'url(/img/gallery/centralparknight.jpg) center / cover' }}>
                                     </CardTitle>
                                     <CardText>
                                     Central Park, NYC, 2020
                 </CardText>
                                     <CardActions border>
-                                        <Button colored img src={"./img/gallery/centralparknight.jpg"}>View Full Size</Button>
+                                        <Button colored href={"./img/gallery/centralparknight.jpg"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
@@ -66,14 +66,14 @@ function Gallery() {
                                 </Card>
                             </Cell>
                             <Cell col={3}>
-                                <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                    <CardTitle style={{ height: '480px', width: '330px', color: 'white', background: 'url(/img/Me.jpg) center / cover' }}>
+                                <Card shadow={5} style={{ maxWidth: '250px', margin: 'auto' }}>
+                                    <CardTitle style={{ height: '300px', width: '250px', color: 'white', background: 'url(/img/gallery/palmtreedusk.jpg) center / cover' }}>
                                     </CardTitle>
                                     <CardText>
-                                    Fort Lauderdale, FL, 2016
+                                    Fort Lauderdale, FL, 2015
                 </CardText>
                                     <CardActions border>
-                                        <Button colored img src={"./img/gallery/palmtreedusk.jpg"}>View Full Size</Button>
+                                        <Button colored href={". /img/gallery/palmtreedusk.jpg"}>View Full Size</Button>
                                     </CardActions>
                                     <CardMenu style={{ color: '#fff' }}>
                                         <IconButton name='share' />
@@ -87,6 +87,11 @@ function Gallery() {
             </div>
         </>
     );
+
+    function enlarge() {
+
+         
+    } 
 };
 
 export default Gallery;
